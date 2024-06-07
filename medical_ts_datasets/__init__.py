a28efa5554385c30299f8598f89cd23ac7c01ced
Rachael DeVries <71657701+gsn245@users.noreply.github.com>
2024-06-07 16:13:11 +0200

    Update __init__.py

--- medical_ts_datasets/__init__.py
+++ medical_ts_datasets/__init__.py
@@ -1,9 +1,9 @@
 """Tensorflow datasets of medical time series."""
-import seft.medical_ts_datasets_radv.medical_ts_datasets.checksums
-import seft.medical_ts_datasets_radv.medical_ts_datasets.physionet_2012
-import seft.medical_ts_datasets_radv.medical_ts_datasets.physionet_2019
-import seft.medical_ts_datasets_radv.medical_ts_datasets.mimic_3_phenotyping
-import seft.medical_ts_datasets_radv.medical_ts_datasets.mimic_3_mortality
+import medical_ts_datasets.checksums
+import medical_ts_datasets.physionet_2012
+import medical_ts_datasets.physionet_2019
+import medical_ts_datasets.mimic_3_phenotyping
+import medical_ts_datasets.mimic_3_mortality
 
 builders = [
     'physionet2012',
